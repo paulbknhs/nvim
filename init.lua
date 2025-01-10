55ff086cfab08f5a3f749bb035e16cf74034a731
paulbknhs <dev@paulbknhs.de>
2025-01-10 14:17:25 +0100

    eee

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- Cycle through buffers with Ctrl+Tab
+vim.api.nvim_set_keymap("n", "<C-Tab>", ":bnext<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-S-Tab>", ":bprevious<CR>", { noremap = true, silent = true })
